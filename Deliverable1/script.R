3061a79b3f297b678a3e31cf6d9a214d5bd27c14
gorka.pinol <gorka.pinol@gmail.com>
2017-09-27 10:49:27 +0200

    solucionat! ole ole

--- Deliverable1/script.R
+++ Deliverable1/script.R
@@ -6,7 +6,5 @@ df<-df[sam,] #selection of 5000 data rows
 df$VendorID<-factor(df$VendorID,labels=c("Creative","VeriFone"))
 df$Trip_type<-factor(df$Trip_type,labels=c("Street-hail","Dispatch"))
 df$Payment_type<-factor(df$Payment_type,labels=c("Credit card","Cash","No charge","Dispute"))
-df$RateCodeID<-factor(df$RateCodeID,labels=c("Standard","JFK","Newark","Nassau/Westchester","Negotiated"))
+df$RateCodeID<-factor(df$RateCodeID,labels=c("Standard","JFK","Newark","Negotiated")) #no trips to Nassau/Westchester
 
-
-#RECORDAR CANVIAR NA's A Payment_type per "No charge" (INVESTIGAR COM)
