c8c9c1a96b2694b12f572024f818dbcd0a9bf6c6
gorka.pinol <gorka.pinol@gmail.com>
2017-10-04 17:14:28 +0200

    things

--- usefulScript.R
+++ usefulScript.R
@@ -20,3 +20,49 @@ df[sel, "Pickup_longitude"]<- NA #de las seleccionadas, le ponemos NA a Pickup_l
 
 # variable cuantitativa: histograma y boxplot
 # variable cualitativa: barplot
+
+df$pickup<-substr(strptime(df$lpep_pickup_datetime, "%Y-%m-%d %H:%M:%S"), 12, 13)
+df$dropoff<-substr(strptime(df$Lpep_dropoff_datetime, "%Y-%m-%d %H:%M:%S"), 12, 13)
+
+na_count <-sapply(df, function(y) sum(length(which(is.na(y)))))
+na_count <- data.frame(na_count)
+
+
+
+#boxplots to do:
+boxplot(df$Pickup_latitude)
+boxplot(df$Pickup_latitude)
+boxplot(df$Pickup_longitude)
+boxplot(df$Dropoff_latitude)
+boxplot(df$Dropoff_longitude)
+boxplot(df$Trip_distance)
+boxplot(df$Fare_amount)
+boxplot(df$Extra)
+boxplot(df$MTA_tax)
+boxplot(df$Tip_amount)
+boxplot(df$Tolls_amount)
+boxplot(df$improvement_surcharge)
+boxplot(df$Total_amount)
+
+#histograms to do:
+hist(df$Pickup_latitude)
+hist(df$Pickup_latitude)
+hist(df$Pickup_longitude)
+hist(df$Dropoff_latitude)
+hist(df$Dropoff_longitude)
+hist(df$Trip_distance)
+hist(df$Fare_amount)
+hist(df$Extra)
+hist(df$MTA_tax)
+hist(df$Tip_amount)
+hist(df$Tolls_amount)
+hist(df$improvement_surcharge)
+hist(df$Total_amount)
+
+#barplots for factors:
+pickupCounts <- table(df$pickup)
+barplot(pickupCounts)
+dropoffCounts <- table(df$dropoff)
+barplot(dropoffCounts)
+triptypeCounts <- table(df$Trip_type)
+barplot(triptypeCounts)
