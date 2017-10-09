50d9a9526d309a768ad9c67020c000482ba7a7a2
gorka.pinol <gorka.pinol@gmail.com>
2017-10-09 11:09:23 +0200

    go go gooo

--- usefulScript.R
+++ usefulScript.R
@@ -78,7 +78,9 @@ barplot(vendorCounts)
 
 df$pickup <- as.factor(df$pickup)
 df$dropoff <- as.factor(df$dropoff)
-#aquí va un summary de totes dades
+#aqu? va un summary de totes dades
+#abans dels summary: sink("arxiu.txt")
+#desprÃ©s dels summary: sink()
 
 df$NAvalues <- 0
 
@@ -118,4 +120,14 @@ sel<-rownames(df[which(df[,"Pickup_longitude"]==0),]) #seleccionamos aparte las 
 
                                       sel<-rownames(df[which(df[,"Total_amount"]==0),]) #seleccionamos aparte las que tienen longitud 0
                                       > df[sel, "Total_amount"]<- "missing"
+sel<-rownames(df[which(df[,"Trip_distance"]>(8.6)),])
+                                      
 
+
+for (i in 1:5000) {
+  for(j in 1:23) {
+    if(identical(df2[i,j], "missing") || identical(df2[i,j], "error") || identical(df2[i,j], "outlier")){
+      df2[i,]$NAvalues <- df2[i,]$NAvalues+1
+    }
+  }
+}
